dd3a817f367e2c5578b8378a5681046836800653
dslater38 <dslater38@gmail.com>
2020-01-15 10:44:18 -0500

    Update ymlBuild.bat

--- win/ymlBuild.bat
+++ win/ymlBuild.bat
@@ -1,3 +1,4 @@
+git submodule update --init --recursive
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\VC\Auxiliary\Build\vcvars64.bat"
 msbuild /verbosity:normal /property:Configuration=ReleaseMT /property:Platform=x86 /t:Build /maxcpucount:2 .\win\external_dependencies\external_dependencies.vcxproj
 if errorlevel 1 exit /b 1
