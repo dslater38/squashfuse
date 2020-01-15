cc918b81438d9f0ac073556a8af264548caa52e1
dslater38 <dslater38@gmail.com>
2020-01-15 16:43:40 -0500

    Zip up teh msi file

--- win/ymlBuild.bat
+++ win/ymlBuild.bat
@@ -4,6 +4,9 @@ msbuild /verbosity:normal /property:Configuration=ReleaseMT /property:Platform=x
 if errorlevel 1 exit /b 1
 msbuild /verbosity:normal /property:Project=squashfs-win /property:Configuration=ReleaseMT /property:Platform=x64 /t:Build /maxcpucount:2 .\win\squashfuse.sln
 if errorlevel 1 exit /b 2
+cd .\win\installer\x64\ReleaseMT
+if errorlevel 1 exit /b 3
+zip squashfs_win.zip squashfs_win.msi
+if errorlevel 1 exit /b 4
 
 exit /b 0
-
