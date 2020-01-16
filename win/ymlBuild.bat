cd0fcf8cbef041504a5a5c7ff53ae6f1c9b31c68
dslater38 <dslater38@gmail.com>
2020-01-15 22:43:23 -0500

    Update ymlBuild.bat

--- win/ymlBuild.bat
+++ win/ymlBuild.bat
@@ -4,9 +4,5 @@ msbuild /verbosity:normal /property:Configuration=ReleaseMT /property:Platform=x
 if errorlevel 1 exit /b 1
 msbuild /verbosity:normal /property:Project=squashfs-win /property:Configuration=ReleaseMT /property:Platform=x64 /t:Build /maxcpucount:2 .\win\squashfuse.sln
 if errorlevel 1 exit /b 2
-cd .\win\installer\x64\ReleaseMT
-if errorlevel 1 exit /b 3
-"c:\Program Files\7-Zip\7z.exe" a squashfs_win.zip squashfs_win.msi
-if errorlevel 1 exit /b 4
 
 exit /b 0
