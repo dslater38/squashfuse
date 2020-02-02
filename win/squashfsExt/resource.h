7f48b82951fada52d49e8add4c6f4d4473cd8687
dslater38 <dslater38@gmail.com>
2020-02-02 10:22:35 -0500

    Implemented an Explorer addin. The explorer addin adds a context menu when right-clicking on a squashfs archive that has a .sqf, .sqfs, or .squashfs extension. The context menu allows mounting the archive on a drive letter, or browsing for a directory.

---
+++ win/squashfsExt/resource.h
@@ -0,0 +1,24 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by squashfsExt.rc
+//
+#define IDS_PROJNAME                    100
+#define IDS_MOUNT_ARCHIVE               101
+#define IDD_MOUNT_SQUASHFS              201
+#define IDC_MAP_DRIVE_LETTER            202
+#define IDC_CHOOSE_DIRECTORY            203
+#define IDC_CHOOSE_DRIVE                204
+#define IDC_CHOOSEDIR_BUTTON            205
+#define IDC_MOUNT_PATH                  206
+#define IDC_LABEL_ARCHIVE               207
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        205
+#define _APS_NEXT_COMMAND_VALUE         32768
+#define _APS_NEXT_CONTROL_VALUE         208
+#define _APS_NEXT_SYMED_VALUE           108
+#endif
+#endif
